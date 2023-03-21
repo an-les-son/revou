1e72441fd0b8e0dfbb28af54ea5cff2a2aa9f492
Anjar Oki Wibowo <anjar.les.son@gmail.com>
2023-03-21 19:03:52 +0700

    update project

--- day 6/dom/index.js
+++ day 6/dom/index.js
@@ -7,6 +7,7 @@
 // document.getElementsByTagName() > digunakan untuk mengambil elemen lebih dari 1 dengan TagName
 // document.querySelector() > digunakan untuk mengambil elemen dengan Selector
 // document.querySelectorAll() > digunakan untuk mengambil elemen lebih dari dengan Selector
+// querySelector juga bisa di pake untuk mengambil id dan class di CSS juga
 
 var header = document.querySelector("h1");
 header.innerHTML = "Header telah dirubah";
@@ -15,3 +16,16 @@ console.log(header);
 var rubah = document.getElementsByClassName("rubah");
 console.log(rubah);
 rubah[2].innerHTML = "Tiga Tigo";
+
+// Event Handling
+// Event adalah kejadian yang terjadi di halaman web salah satunya adalah aktivitas yang dikerjakan oleh user pada halaman web. Kita bisa melakukan aksi tertentu untuk merespon event tersebut.
+// conto event yang dilakukan seperti:
+// - user melakukan scroll
+// - user melakukan klik pada elemen tertentu
+// - halaman web di-load
+// - form di-submit
+
+var succesBtn = document.getElementById("succes");
+succesBtn.addEventListener("click", function () {
+  console.log("masuk succes");
+});
--- project/js/script.js
+++ project/js/script.js
@@ -1,18 +1,63 @@
-// let button = document.getElementById("hitung");
+let button = document.getElementById("hitung");
+let btnReset = document.getElementById("reset");
 
-// button.addEventListener("click", (event) => {
-//   const jenisKelamin = document.querySelectorAll("input[name='jenis-kelamin']:checked").value;
-//   const berat = document.getElementById("berat").value;
-//   const usia = document.getElementById("usia").value;
-//   const tinggi = document.getElementById("tinggi").value;
+function hitungBMI() {
+  // varibel yang di gunakan untuk menghitung
+  let berat = document.getElementById("berat").value;
+  let tinggi = document.getElementById("tinggi").value;
 
-//   console.log(jenisKelamin);
-//   console.log(berat);
-//   console.log(usia);
-//   console.log(tinggi);
-// });
+  let t = Math.pow(tinggi / 100, 2);
+  let h1 = berat / t;
+  let a = h1.toFixed(1);
 
-let findSelected = () => {
-  let selected = document.querySelector("input[name='jenis-kelamin']:checked").value;
-  console.log(selected);
-};
+  // varibel yang di gunakan untuk output
+  let s = document.getElementById("status");
+  let bmi = document.getElementById("hasil");
+  let k1 = document.getElementById("keterangan-1");
+  const p1 = document.getElementById("penjelasan1");
+  const p2 = document.getElementById("penjelasan2");
+  const p3 = document.getElementById("penjelasan3");
+  const p4 = document.getElementById("penjelasan4");
+  const s1 = document.getElementById("penyakit1");
+  const s2 = document.getElementById("penyakit2");
+
+  if (a <= 18.5) {
+    s.innerHTML = "Kekurangan berat badan";
+    k1.innerHTML = "Anda kekurangan berat badan";
+    bmi.innerHTML = a;
+    p1.classList.remove("disable");
+    s1.classList.remove("disable");
+  } else if (a > 18.5 && a <= 24.9) {
+    s.innerHTML = "Normal";
+    k1.innerHTML = "Anda memiliki berat badan ideal.<br>Good job!!";
+    bmi.innerHTML = a;
+    p2.classList.remove("disable");
+  } else if (a >= 25 && a <= 29.9) {
+    s.innerHTML = "Kelebihan berat badan";
+    k1.innerHTML = "Anda memiliki berat badan berlebih";
+    bmi.innerHTML = a;
+    p3.classList.remove("disable");
+    s2.classList.remove("disable");
+  } else if (a >= 30) {
+    s.innerHTML = "Obesitas";
+    k1.innerHTML = "Anda berada dalam kategori obesitas";
+    bmi.innerHTML = a;
+    p4.classList.remove("disable");
+    s2.classList.remove("disable");
+  }
+}
+
+function getReset() {
+  document.getElementById("berat").value = "";
+  document.getElementById("usia").value = "";
+  document.getElementById("tinggi").value = "";
+}
+
+function display() {
+  document.getElementById("p-atas").classList.add("p-hilang");
+  document.getElementById("p-bawah").classList.add("p-hasil");
+}
+
+button.addEventListener("click", hitungBMI);
+button.addEventListener("click", display);
+btnReset.addEventListener("click", getReset);
