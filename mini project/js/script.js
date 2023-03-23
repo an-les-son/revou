47c0a29b336ccc2e321b5545a7bde344c157f6ce
Anjar Oki Wibowo <anjar.les.son@gmail.com>
2023-03-23 08:06:16 +0700

    update mini project

--- mini project/js/script.js
+++ mini project/js/script.js
@@ -1,3 +1,4 @@
+// varibel tombol "Hitung BMI" dan "Reset"
 let button = document.getElementById("hitung");
 let btnReset = document.getElementById("reset");
 
@@ -6,9 +7,10 @@ function hitungBMI() {
   let berat = document.getElementById("berat").value;
   let tinggi = document.getElementById("tinggi").value;
 
-  let t = Math.pow(tinggi / 100, 2);
-  let h1 = berat / t;
-  let a = h1.toFixed(1);
+  // rumus perhitungan BMI
+  let t = Math.pow(tinggi / 100, 2); /* memberi varial bantuan yaitu t, untuk menampung nilai tinggi badan yang dibuat ke meter dahulu, lalu di pangkatkan 2 */
+  let h = berat / t; /* memberi varibel bantuan yaitu h, untuk menampung nilai berat badan yang di bagi dengan variabel t , sebenarnya hasilnya sudah merupakan hasil perhitungan BMI tetapi di belakang koma masih banyak digit angka*/
+  let a = h.toFixed(1); /* memberi varibel bantuan yaitu a, hasil final perhitungan BMI dengan angka 1 digit dibelakang koma */
 
   // varibel yang di gunakan untuk output
   let s = document.getElementById("status");
@@ -21,12 +23,13 @@ function hitungBMI() {
   const s1 = document.getElementById("penyakit1");
   const s2 = document.getElementById("penyakit2");
 
+  // perbandingan if else untuk menampilkan hasil dan beberapa penjelasan mengenai hasil yang di dapet dari perhitungan BMI
   if (a <= 18.5) {
-    s.innerHTML = "Kekurangan berat badan";
-    k1.innerHTML = "Anda kekurangan berat badan";
-    bmi.innerHTML = a;
-    p1.classList.remove("disable");
-    s1.classList.remove("disable");
+    s.innerHTML = "Kekurangan berat badan"; /* menambahkan text html di variabel s */
+    k1.innerHTML = "Anda kekurangan berat badan"; /* menambahkan text html variabel k1*/
+    bmi.innerHTML = a; /* menambahkan nilai a hasil perhitungan yang di atas di variabel bmi */
+    p1.classList.remove("disable"); /* menghapus class disable yang berada di p1 */
+    s1.classList.remove("disable"); /* menghapus class disable yang berada di s1 */
   } else if (a > 18.5 && a <= 24.9) {
     s.innerHTML = "Normal";
     k1.innerHTML = "Anda memiliki berat badan ideal<br>Good job!!";
@@ -47,28 +50,32 @@ function hitungBMI() {
   }
 }
 
+// function reset untuk menghilangkan value input form yang sudah di isikan
 function getReset() {
   document.getElementById("berat").value = "";
   document.getElementById("usia").value = "";
   document.getElementById("tinggi").value = "";
 }
 
+// function display untuk pengaturan tampilan hilang dan tampil dengan menambahkan class di dalam element-nya
 function display() {
   document.getElementById("p-atas").classList.add("p-hilang");
   document.getElementById("p-bawah").classList.add("p-hasil");
 }
 
+// function untuk menampilkan pilihan input bertype radio
 function getRadio() {
   var radio = document.getElementsByName("jenis-kelamin");
   var i;
   for (i = 0; i <= radio.length; i++) {
     if (radio[i].checked) {
       document.getElementById("radio-output").innerHTML =
-        "BMI :<br>" + radio[i].value + " Usia " + document.getElementById("usia").value + " Thn" + "<br>Berat " + document.getElementById("berat").value + " Kg" + "<br>Tinggi " + document.getElementById("tinggi").value + " Cm";
+        radio[i].value + "<br>Usia " + document.getElementById("usia").value + " Thn" + "<br>Berat " + document.getElementById("berat").value + " Kg" + "<br>Tinggi " + document.getElementById("tinggi").value + " Cm";
     }
   }
 }
 
+// ini membuat perintah kepada stiap variabel tombol, setelah di click dan menjalankan function yang ada
 button.addEventListener("click", hitungBMI);
 button.addEventListener("click", display);
 button.addEventListener("click", getRadio);
